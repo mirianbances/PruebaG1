80e7315ab41bcfc1f3a5d158d8e2bc22370561c9
Mirian <Mirian@192.168.94.221>
2015-03-18 10:32:21 -0500

    se creo la clase1

---
+++ PruebaLp2G1/src/pruebalp2g1/clase1.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebalp2g1;
+
+
+public class clase1 {
+    String nombre;
+    int i;
+}
